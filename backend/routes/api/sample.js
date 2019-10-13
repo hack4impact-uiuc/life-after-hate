2e803d13d3ffaa02de83386f1adeac1165b6e4e1
Josh Byster <joshb1050@gmail.com>
2019-10-13 01:43:01 -0500

    Fix prettier and globbing

--- backend/routes/api/sample.js
+++ backend/routes/api/sample.js
@@ -1,8 +1,8 @@
-var router = require('express').Router();
+var router = require("express").Router();
 
 // Corresopnds to a GET request to /api/sample/
-router.get('/', function(req, res, next){
-    res.send("Hello world!");
+router.get("/", function(req, res, next) {
+  res.send("Hello world!");
 });
 
 module.exports = router;
