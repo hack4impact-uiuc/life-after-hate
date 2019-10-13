32033ed57ace6e3a78d50f81da0b463a9c8c5712
Josh Byster <joshb1050@gmail.com>
2019-10-13 00:40:52 -0500

    Added mongo scaffolding

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = process.env.PORT
-
 app.use(require('./routes'));
 
+mongoose.connect('mongodb://db:27017/LAH_DB', { useUnifiedTopology: true, useNewUrlParser: true });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
