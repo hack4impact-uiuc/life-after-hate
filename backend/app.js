f98046218dc5d222e56b0f15d7130a1c65a01154
Josh Byster <joshb1050@gmail.com>
2019-10-13 01:18:01 -0500

    Ran prettier

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const app = express()
-const port = process.env.PORT
-app.use(require('./routes'));
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = process.env.PORT;
+app.use(require("./routes"));
 
-mongoose.connect('mongodb://db:27017/LAH_DB', { useUnifiedTopology: true, useNewUrlParser: true });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+mongoose.connect("mongodb://db:27017/LAH_DB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
-var router = require('express').Router();
+var router = require("express").Router();
 
-router.use('/api', require('./api'));
+router.use("/api", require("./api"));
 
 module.exports = router;
