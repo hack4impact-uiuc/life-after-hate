f949f47e63358657553fe5a082ec41b6dcd9b7d6
Josh Byster <joshb1050@gmail.com>
2019-10-16 02:23:35 -0500

    Added test

---
+++ backend/test/example.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe("Example", function() {
+  describe("example test", function() {
+    it("should return 2 upon 1+1", function() {
+      assert.equal(1 + 1, 2);
+    });
+  });
+});
