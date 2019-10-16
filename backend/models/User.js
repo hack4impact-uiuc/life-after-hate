f74522a6e4f0b90aaff87163ec050afba0c9f377
Alice Fang <32147571+alicesf2@users.noreply.github.com>
2019-10-15 22:14:19 -0500

    created user model (#9)

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+/** Schema representing a LAH user
+ * Role should be one of the following: [Admin, Volunteer]
+ */
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  oauthId: { type: String, required: true, unique: true },
+  propicUrl: { type: String, required: true },
+  isApproved: { type: Boolean, required: true },
+  role: {
+    type: String,
+    enum: ["ADMIN", "VOLUNTEER"],
+    required: true
+  },
+  location: {
+    type: String,
+    enum: ["NORTH", "SOUTH"],
+    required: true
+  }
+});
+
+module.exports = mongoose.model("User", User);
