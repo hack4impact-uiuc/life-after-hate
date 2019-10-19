f2e8f010ed9a56fe95697ff861532bd94aaa81ca
Albert Cao <albertc4@illinois.edu>
2019-10-19 12:38:12 -0500

    Added markers to be passed in as props to the map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,14 @@ import "./styles/App.scss";
 
 import MapView from "./pages/mapView.js";
 
+const markers = [
+  { latitude: 38.2, longitude: -122.4 },
+  { latitude: 38.9, longitude: -123.1 }
+];
 function App() {
   return (
     <div className="App">
-      <MapView />
+      <MapView markers={markers} />
     </div>
   );
 }
--- frontend/src/pages/mapView.js
+++ frontend/src/pages/mapView.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
+import "../styles/mapView.scss";
 
 class MapView extends React.Component {
   state = {
     viewport: {
-      width: 400,
-      height: 400,
+      width: 800,
+      height: 800,
       latitude: 37.7577,
       longitude: -122.4376,
       zoom: 8
@@ -19,7 +20,18 @@ class MapView extends React.Component {
           {...this.state.viewport}
           onViewportChange={viewport => this.setState({ viewport })}
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        />
+        >
+          {this.props.markers.map(item => (
+            <Marker
+              className="marker"
+              key={item.id}
+              latitude={item.latitude}
+              longitude={item.longitude}
+              offsetLeft={-20}
+              offsetTop={-10}
+            />
+          ))}
+        </ReactMapGL>
       </div>
     );
   }
