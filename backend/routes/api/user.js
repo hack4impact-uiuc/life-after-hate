4d34a28c913c633dc8581d39a103a1f4a92c2a0d
Alice Fang <alicesf2@illinois.edu>
2019-10-20 18:14:08 -0500

    added create new user function and set default approval to false

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("../../models/User");
+const User = require("../../models/User");
 
 // get all users
 router.get("/", async (req, res) => {
-  const users = await User.find();
+  const users = await User.find({});
   res.json({
     code: 200,
     result: users,
+    success: true
+  });
+});
+
+// create new user
+router.post("/", async (req, res) => {
+  const data = req.body;
+  const newUser = new User({
+    firstName: data.firstName,
+    lastName: data.lastName,
+    oauthId: data.oauthId,
+    propicUrl: data.propicUrl,
+    role: data.role,
+    location: data.location
+  });
+  await newUser.save();
+  res.json({
+    code: 200,
+    message: "User Successfully Created",
     success: true
   });
 });
