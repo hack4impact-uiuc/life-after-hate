b3132f67e055c09c9d14d4f5a9a89e1a56323ea4
Josh Byster <8664074+josh-byster@users.noreply.github.com>
2019-10-22 21:46:50 -0500

    Added joi (#25)

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,22 @@ const mongoose = require("mongoose");
 const app = express();
 const passport = require("passport");
 const morgan = require("morgan");
+const { errors } = require("celebrate");
+const bodyParser = require("body-parser");
+
 require("./utils/passport-setup");
 
 app.use(morgan("dev"));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.json());
 app.use(require("./routes"));
 
 mongoose.connect(process.env.DB_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true
 });
+
+app.use(errors());
 
 module.exports = app;
