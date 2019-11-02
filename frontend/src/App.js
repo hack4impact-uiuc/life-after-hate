fa96a4be1aad30f1711597582f419a078d1e04b4
Albert Cao <40071557+abetco@users.noreply.github.com>
2019-11-01 20:50:48 -0500

    Created resource cards to display on search submit (#34)
    * Created resource card
    
    * Created div for search results
    
    * Removed commented sections and extraneous files
    
    * changed card to div
    
    * Added styling for cards
    
    * Changed single source of information to the mapview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+// import MapView from "./pages/MapView";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
+      {/* <MapView /> */}
       <Login />
     </div>
   );
