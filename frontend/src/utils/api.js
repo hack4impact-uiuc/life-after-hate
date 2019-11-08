dc885f72cfacb656c92a87948712c6696b640832
Albert Cao <albertc4@illinois.edu>
2019-11-08 02:23:31 -0600

    Fixed error handling bugs

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,3 @@
-// import errorHandler from "./errorHandler";
-
 require("node-fetch");
 
 const API_URI = process.env.REACT_APP_API_URI
@@ -8,51 +6,49 @@ const API_URI = process.env.REACT_APP_API_URI
 
 const host = `${API_URI}/api`;
 
-async function getEndPoint(endPoint) {
+async function fetchWithError(
+  endPoint,
+  method,
+  data = null,
+  additional_headers = null
+) {
+  let headers = {};
+  if (!(method === "GET" || method === "DELETE")) {
+    headers = { ...additional_headers, "Content-Type": "application/json" };
+  }
   let response = await fetch(`${host}/${endPoint}`, {
-    method: "GET"
+    method,
+    headers,
+    body: !method === "GET" || (method === "DELETE" && JSON.stringify(data))
   });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
   let responseJson = await response.json();
   return responseJson.result;
+}
+
+async function getEndPoint(endPoint) {
+  let responseJsonResult = await fetchWithError(endPoint, "GET");
+  return responseJsonResult;
 }
 
 /**
 async function postEndPoint(endPoint, data, additonal_headers = null) {
-  let headers = { ...additonal_headers, "Content-Type": "application/json" };
-  let response = await fetch(`${host }/${ endPoint}`, {
-    method: "POST",
-    headers,
-    body: JSON.stringify(data)
-  });
-  let responseJson = await response.json();
-  return responseJson.result;
+  return await fetchWithError(endPoint, "POST", data, additional_headers = additional_headers);
 }
 
 async function putEndPoint(endPoint, data, additonal_headers = null) {
-  let headers = { ...additonal_headers, "Content-Type": "application/json" };
-  let response = await fetch(`${host }/${ endPoint}`, {
-    method: "PUT",
-    headers,
-    body: JSON.stringify(data)
-  });
-  let responseJson = await response.json();
-  return responseJson.result;
+  return await fetchWithError(endPoint, "PUT", data, additional_headers = additional_headers);
 }
 
 async function deleteEndPoint(endPoint) {
-    let response = await fetch(`${host }/${ endPoint}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-    let responseJson = await response.json();
-    return responseJson.result;
+    return await fetchWithError(endPoint, "DELETE");
 }
 */
 
-async function getSearchResults() {
-  return await getEndPoint(`resources/${123}`, "");
+async function getSearchResults(keyword) {
+  return await getEndPoint(`resources/filter?keyword=${keyword}`, "");
 }
 
 export { getSearchResults };
