5a9310290ae2fd0d8c085153bc714c6b9d4ec31b
Josh Byster <8664074+josh-byster@users.noreply.github.com>
2019-11-14 19:23:30 -0600

    Auth Stubbing for Middleware (#65)
    * Added auth stubbing for middleware
    
    * Pulled in changes on Gene's PR

---
+++ backend/test/auth_stubs.js
@@ -0,0 +1,37 @@
+const sinon = require("sinon");
+const auth = require("../utils/auth-middleware");
+
+// The default method that will run if no fn is specified
+// Essentially just bypassing the middleware by calling next()
+const defaultStub = (req, res, next) => {
+  next();
+};
+
+// Take the name of the auth method along with the function
+// and stub the result with fn
+const stubMiddleware = (name, fn = defaultStub) => {
+  const stub = sinon.stub(auth, name);
+  stub.callsFake(fn);
+  return stub;
+};
+
+const stubIsAdmin = fn => stubMiddleware("isAdmin", fn);
+
+const stubIsAuthenticated = fn => stubMiddleware("isAuthenticated", fn);
+
+const stubIsVolunteer = fn => stubMiddleware("isVolunteer", fn);
+
+// Stub all the auth middleware functions
+// Return an object with all the stubbed function references
+const stubAllAuth = fn => ({
+  isAdminStub: stubIsAdmin(fn),
+  isAuthenticatedStub: stubIsAuthenticated(fn),
+  isVolunteerStub: stubIsVolunteer(fn)
+});
+
+module.exports = {
+  stubIsAdmin,
+  stubIsAuthenticated,
+  stubIsVolunteer,
+  stubAllAuth
+};
