ed316e56290ba222a1cee387c98068b500518665
Josh Byster <joshb1050@gmail.com>
2019-11-16 04:18:21 -0600

    Fixed up manifest and metadata

--- backend/utils/error-handler.js
+++ backend/utils/error-handler.js
@@ -1,5 +1,5 @@
 const Boom = require("@hapi/boom");
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   console.error(err);
   res.status(500).send(Boom.badImplementation("terrible implementation"));
 };
