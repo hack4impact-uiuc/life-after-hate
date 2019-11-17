ba1f93822aa45a62ec68a6fbf741c5b78f84e66c
Josh Byster <joshb1050@gmail.com>
2019-11-17 05:16:44 -0600

    Added to lahutil

--- backend/utils/generate_mock_data.js
+++ backend/utils/generate_mock_data.js
@@ -1,12 +1,13 @@
 const fetch = require("node-fetch");
 const mongoose = require("mongoose");
-
-const Resource = require("./models/Resource");
+const colors = require("colors");
+const Resource = require("../models/Resource");
 
 const JSON_LINK =
   "http://www.json-generator.com/api/json/get/cfakcpbucy?indent=2";
 
 const createConnection = async () => {
+  console.log(colors.green("Attempting to connect to Mongo..."));
   await mongoose.connect(process.env.DB_URI, {
     useUnifiedTopology: false,
     useNewUrlParser: true
@@ -14,26 +15,30 @@ const createConnection = async () => {
 };
 
 const closeConnection = async () => {
+  console.log(colors.green("Closing server connection..."));
   await mongoose.connection.close();
 };
 
 const fetchJson = async () => {
+  console.log(colors.green("Fetching JSON..."));
   const response = await fetch(JSON_LINK);
   const dataJSON = await response.json();
   return dataJSON;
 };
 
-const addSampleResource = async resource => {
+const addSampleResource = resource => {
   const newResource = new Resource(resource);
-  await newResource.save();
+  return newResource.save();
 };
 
 const main = async () => {
   await createConnection();
-  const dataJSON = await fetchJson();
-  for (var i = 0; i < dataJSON.length; i++) {
-    await addSampleResource(dataJSON[i]);
-  }
+  console.log(colors.green("Clearing all existing resource data..."));
+  await Resource.deleteMany({});
+  const data = await fetchJson();
+  let resources = data.map(addSampleResource);
+  console.log(colors.green("Saving mock data in DB..."));
+  await Promise.all(resources);
   await closeConnection();
 };
 
