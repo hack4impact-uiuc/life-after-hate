6fff757720ed07708f36f25c274594e523ecc6aa
gwang0136 <genew2@illinois.edu>
2019-11-21 21:47:41 -0600

    minor change to remove duplicate tags

--- backend/routes/api/resources.js
+++ backend/routes/api/resources.js
@@ -92,7 +92,7 @@ router.post(
       language: "english",
       remove_digits: true,
       return_changed_case: true,
-      remove_duplicates: false
+      remove_duplicates: true
     });
     const newResource = new Resource(data);
     await newResource.save();
