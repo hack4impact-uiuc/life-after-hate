581a857c9e6f71f4f32529031f8fd31a9150a66b
Rebecca Xun <rebeccaxun@gmail.com>
2019-11-21 19:36:58 -0600

    geocoding

--- backend/routes/api/resources.js
+++ backend/routes/api/resources.js
@@ -4,8 +4,41 @@ const Resource = require("../../models/Resource");
 const errorWrap = require("../../utils/error-wrap");
 const { celebrate, Joi } = require("celebrate");
 const Fuse = require("fuse.js");
+const fetch = require("node-fetch");
 const { sortByDistance, options } = require("../../utils/resource-utils");
 Joi.objectId = require("joi-objectid")(Joi);
+
+async function addressToLatLong(address) {
+  let api_latlong = `http://www.mapquestapi.com/geocoding/v1/address?key=AAAppgYd4dZiZKW2sAtBtgEUAYejofok&maxResults=5&outFormat=json&location=${address}`;
+  // "&boundingBox=40.121581,-88.253981,40.098315,-88.205082";
+
+  const response = await fetch(api_latlong, {});
+  const responseJson = await response.json();
+
+  let lat = responseJson["results"][0]["locations"][0]["latLng"]["lat"];
+  let lng = responseJson["results"][0]["locations"][0]["latLng"]["lng"];
+  return [lat, lng];
+}
+
+// async function latlongToAddress(lat, long) {
+//   let api_address = `http://www.mapquestapi.com/geocoding/v1/reverse?key=AAAppgYd4dZiZKW2sAtBtgEUAYejofok&location=${lat},${long}&includeRoadMetadata=false&includeNearestIntersection=false`;
+//   // "&boundingBox=40.121581,-88.253981,40.098315,-88.205082";
+
+//   const response = await fetch(api_address, {});
+//   const responseJson = await response.json();
+
+//   console.log(responseJson["results"][0]["locations"]);
+
+//   let street_address = responseJson["results"][0]["locations"][0]["street"];
+//   let city = responseJson["results"][0]["locations"][0]["adminArea5"];
+//   let state = responseJson["results"][0]["locations"][0]["adminArea3"];
+//   // country = responseJson["results"][0]["locations"][0]["adminArea1"];
+//   let postal_code = responseJson["results"][0]["locations"][0][
+//     "postalCode"
+//   ].substring(0, 5);
+//   let full_address = `${street_address} ${city} ${state} ${postal_code}`;
+//   return full_address;
+// }
 
 // get all resources
 router.get(
@@ -86,9 +119,22 @@ router.post(
     })
   }),
   errorWrap(async (req, res) => {
+    // const newResource;
     const data = req.body;
+    const latlng = await addressToLatLong(data.address);
+
+    data.location.coordinates[0] = latlng[0];
+    data.location.coordinates[1] = latlng[1];
+
+    // const goodAddress = latlongToAddress(data.location.coordinates[0], data.location.coordinates[1]);
+    // goodAddress.then(function(result) {
+    //   data.address = result;
+    //   console.log("result is " + result);
+    // })
+
     const newResource = new Resource(data);
     await newResource.save();
+
     res.json({
       code: 201,
       message: "Resource Successfully Created",
