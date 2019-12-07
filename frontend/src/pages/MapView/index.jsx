98022ccd4d6b6b62cb7263afc33b53958c13a628
Josh Byster <8664074+josh-byster@users.noreply.github.com>
2019-12-07 01:36:04 -0600

    Changed to layering system (#115)
    * Very basic layering with Deck
    
    * Finished layer
    
    * Cleanup
    
    * Added comment and PureComponent

--- frontend/src/pages/MapView/index.jsx
+++ frontend/src/pages/MapView/index.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import StaticMap, { Popup } from "react-map-gl";
 
 import { getSearchResults } from "../../utils/api";
-import Pin from "../../components/Pin";
 import ResourceCard from "../../components/ResourceCard";
 import Search from "../../components/SearchBar";
 import "./styles.scss";
+import DeckGL from "@deck.gl/react";
+import { IconLayer } from "@deck.gl/layers";
+import MarkerImg from "../../assets/images/marker.png";
 
-const pinSize = 35;
-
+const pinSize = 45;
 const searchSuggestions = [];
+// Mapping used for IconAtlas, which is not really being used fully currently,
+// As we're only rendering one type of custom icon for all points of interest
+const mapping = {
+  marker: {
+    x: 0,
+    y: 0,
+    width: 360,
+    height: 512,
+    anchorY: 512
+  }
+};
+
+const INITIAL_VIEW_STATE = {
+  longitude: -35,
+  latitude: 36.7,
+  zoom: 1.8,
+  maxZoom: 20,
+  pitch: 0,
+  bearing: 0
+};
 
 class MapView extends Component {
   constructor(props) {
@@ -30,33 +51,40 @@ class MapView extends Component {
     };
   }
 
-  renderCards = card => (
-    <ResourceCard
-      name={card.companyName}
-      description={card.description}
-      tags={card.tags}
-      contactName={card.contactName}
-      contactPhone={card.contactPhone}
-      contactEmail={card.contactEmail}
-      address={card.address}
-      notes={card.notes}
-    />
-  );
+  getLayers = () => {
+    const data = this.state.searchResults;
+    const layerProps = {
+      data,
+      pickable: true,
+      wrapLongitude: true,
+      getPosition: d => d.location.coordinates,
+      iconAtlas: MarkerImg,
+      iconMapping: mapping
+    };
 
-  renderMarkers = marker => (
-    <Marker
-      key={marker.id}
-      longitude={marker.location.coordinates[0]}
-      latitude={marker.location.coordinates[1]}
-    >
-      <Pin
-        size={pinSize}
-        onClick={() => {
-          this.setState({ popup: marker });
-        }}
+    const layer = new IconLayer({
+      ...layerProps,
+      id: "icon",
+      getIcon: () => "marker",
+      sizeUnits: "meters",
+      sizeMinPixels: pinSize
+    });
+
+    return [layer];
+  };
+  renderCards = card => (
+      <ResourceCard
+        key={card._id}
+        name={card.companyName}
+        description={card.description}
+        tags={card.tags}
+        contactName={card.contactName}
+        contactPhone={card.contactPhone}
+        contactEmail={card.contactEmail}
+        address={card.address}
+        notes={card.notes}
       />
-    </Marker>
-  );
+    );
 
   searchHandler = async () => {
     let searchResults;
@@ -109,36 +137,45 @@ class MapView extends Component {
             </div>
           )}
         </div>
-        <ReactMapGL
-          {...this.state.viewport}
-          width="100%"
-          height="100vh"
-          onViewportChange={viewport => this.setState({ viewport })}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        <DeckGL
+          layers={this.getLayers()}
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={{ dragRotate: false }}
+          onHover={e => {
+            this.setState({ popup: e.object });
+          }}
         >
-          {this.state.showResults &&
-            this.state.searchResults &&
-            this.state.searchResults.map(this.renderMarkers)}
-          {this.state.popup && (
-            <Popup
-              latitude={this.state.popup.location.coordinates[1]}
-              longitude={this.state.popup.location.coordinates[0]}
-              tipSize={5}
-              closeOnClick={false}
-              dynamicPosition={true}
-              offsetTop={-27}
-              onClose={() => this.setState({ popup: null })}
-            >
-              <div className="popup">
-                <div className="popup-title">
-                  {this.state.popup.companyName}
+          <StaticMap
+            {...this.state.viewport}
+            width="100%"
+            height="100vh"
+            onViewportChange={viewport => this.setState({ viewport })}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          >
+            {this.state.popup && (
+              <Popup
+                latitude={this.state.popup.location.coordinates[1]}
+                longitude={this.state.popup.location.coordinates[0]}
+                tipSize={5}
+                closeOnClick={false}
+                dynamicPosition={true}
+                offsetTop={-27}
+                captureScroll={false}
+                onClose={() => this.setState({ popup: null })}
+              >
+                <div className="popup">
+                  <div className="popup-title">
+                    {this.state.popup.companyName}
+                  </div>
+                  <div className="popup-distance">0.5 miles away</div>
+                  <div className="popup-desc">
+                    {this.state.popup.description}
+                  </div>
                 </div>
-                <div className="popup-distance">0.5 miles away</div>
-                <div className="popup-desc">{this.state.popup.description}</div>
-              </div>
-            </Popup>
-          )}
-        </ReactMapGL>
+              </Popup>
+            )}
+          </StaticMap>
+        </DeckGL>
       </div>
     );
   }
