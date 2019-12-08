1a7c31b1264fe9d4393b15ab76f58615c79bbfcc
Lauren Ho <laurenh4@illinois.edu>
2019-12-07 19:10:38 -0600

    Directory search by tag and location

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,16 +4,24 @@ import {
   purgeGlobalAuthState
 } from "./apiHelpers";
 
-async function getSearchResults(keyword, address) {
+async function getSearchResults(keyword, address, tag) {
   const defaultRadius = 500;
   let endptStr = `resources/filter?`;
 
-  if (address && keyword) {
+  if (address && keyword && tag) {
+    endptStr += `radius=${defaultRadius}&address=${address}&keyword=${keyword}&tag=${tag}`;
+  } else if (address && keyword) {
     endptStr += `radius=${defaultRadius}&address=${address}&keyword=${keyword}`;
+  } else if (address && tag) {
+    endptStr += `radius=${defaultRadius}&address=${address}&tag=${tag}`;
+  } else if (keyword && tag) {
+    endptStr += `radius=${defaultRadius}&keyword=${keyword}&tag=${tag}`;
   } else if (address) {
     endptStr += `radius=${defaultRadius}&address=${address}`;
-  } else {
+  } else if (keyword) {
     endptStr += `keyword=${keyword}`;
+  } else {
+    endptStr += `tag=${tag}`;
   }
 
   return (await apiRequest({
