c00140f8d49b79225dcd9d8fc5903e0a569764f2
Josh Byster <joshb1050@gmail.com>
2020-02-16 05:00:40 -0600

    Basic virtualization

--- frontend/src/pages/MapView/CardView/index.jsx
+++ frontend/src/pages/MapView/CardView/index.jsx
@@ -3,6 +3,18 @@ import { connect } from "react-redux";
 import { resourceSelector } from "../../../redux/selectors/resource";
 import ResourceCard from "../ResourceCard";
 import { mapResourceIdSelector } from "../../../redux/selectors/map";
+import {
+  CellMeasurer,
+  List,
+  AutoSizer,
+  CellMeasurerCache
+} from "react-virtualized";
+
+const cache = new CellMeasurerCache({
+  fixedWidth: true,
+  defaultWidth: 324,
+  defaultHeight: 130
+});
 
 class CardView extends React.Component {
   constructor() {
@@ -30,18 +42,75 @@ class CardView extends React.Component {
     );
   };
 
-  componentDidUpdate() {
+  componentDidUpdate(prevProps) {
+    if (prevProps.selectedResource) {
+      const prevResourceIdx = this.props.resources.findIndex(
+        r => r._id === prevProps.selectedResource
+      );
+      cache.clear(prevResourceIdx, 0);
+      this.list.recomputeRowHeights();
+      console.log("CLEARED");
+    }
     if (this.props.selectedResource) {
-      this.state.cardRefs
-        .get(this.props.selectedResource)
-        .current.scrollIntoView({ block: "start", behavior: "smooth" });
+      const currResourceIdx = this.props.resources.findIndex(
+        r => r._id === this.props.selectedResource
+      );
+      cache.clear(currResourceIdx, 0);
+      this.list.recomputeRowHeights();
+      console.log(cache);
+      this.list.scrollToRow(currResourceIdx);
     }
   }
+
+  rowRenderer({ index, key, parent, style }) {
+    // const source // This comes from your list data
+    return (
+      <CellMeasurer
+        cache={cache}
+        columnIndex={0}
+        key={key}
+        parent={parent}
+        rowIndex={index}
+      >
+        {({ registerChild }) => (
+          // 'style' attribute required to position cell (within parent List)
+          <ResourceCard
+            key={this.props.resources[index]._id}
+            ref={registerChild}
+            resource={this.props.resources[index]}
+            isSelected={
+              this.props.resources[index]._id === this.props.selectedResource
+            }
+            style={style}
+          />
+          // <div ref={registerChild} style={style}>
+          //   "Hi"
+
+          // </div>
+        )}
+      </CellMeasurer>
+    );
+  }
+
   render() {
     return (
       this.props.resources.length > 0 && (
         <div className="card-content">
-          {this.props.resources.map(this.renderCard)}
+          <AutoSizer>
+            {({ height, width }) => (
+              <List
+                width={width}
+                height={height}
+                ref={list => {
+                  this.list = list;
+                }}
+                deferredMeasurementCache={cache}
+                rowHeight={cache.rowHeight}
+                rowRenderer={this.rowRenderer.bind(this)}
+                rowCount={this.props.resources.length}
+              />
+            )}
+          </AutoSizer>
         </div>
       )
     );
