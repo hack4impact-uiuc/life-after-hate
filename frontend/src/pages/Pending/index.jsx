273ad4a848bca4bd44acbafe06842a8b3548569d
Aryn Harmon <achcello@gmail.com>
2020-02-17 16:31:21 -0600

    correct import error which caused a build failure
     * oop

--- frontend/src/pages/Pending/index.jsx
+++ frontend/src/pages/Pending/index.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
 
-// import Logo from "./images/lah-logo.png";
-
-import "./styles.scss";
-
 class Pending extends Component {
   render() {
     return <div>lets pretend we dont exist</div>;
