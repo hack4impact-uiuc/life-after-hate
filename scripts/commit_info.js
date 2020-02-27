704a9c67a3f98925ffd02b46d9ed3634a59b2735
Josh Byster <joshb1050@gmail.com>
2020-02-27 02:41:42 -0600

    Fixed scraper

--- scripts/commit_info.js
+++ scripts/commit_info.js
@@ -1,17 +1,13 @@
 const { commitInfo } = require("@cypress/commit-info");
 commitInfo().then(info => {
-  console.log(`COMMIT_INFO_BRANCH=${JSON.stringify(info.branch)}`);
-  console.log(`COMMIT_INFO_MESSAGE=${JSON.stringify(info.message)}`);
-  console.log(`COMMIT_INFO_EMAIL=${JSON.stringify(info.email)}`);
-  console.log(`COMMIT_INFO_AUTHOR=${JSON.stringify(info.author)}`);
-  console.log(`COMMIT_INFO_SHA=${JSON.stringify(info.sha)}`);
-  console.log(`COMMIT_INFO_REMOTE=${JSON.stringify(info.remote)}`);
-  // info object will have properties
-  // branch
-  // message
-  // email
-  // author
-  // sha
-  // timestamp (in seconds since epoch)
-  // remote
+  console.log(`COMMIT_INFO_BRANCH=${process.env.GITHUB_REF.substring(11)}`);
+  console.log(
+    `COMMIT_INFO_MESSAGE=${JSON.stringify(
+      info.message.replace(/(\r\n|\n|\r)/gm, "")
+    ).slice(1, -1)}`
+  );
+  console.log(`COMMIT_INFO_EMAIL=${JSON.stringify(info.email).slice(1, -1)}`);
+  console.log(`COMMIT_INFO_AUTHOR=${JSON.stringify(info.author).slice(1, -1)}`);
+  console.log(`COMMIT_INFO_SHA=${JSON.stringify(info.sha).slice(1, -1)}`);
+  console.log(`COMMIT_INFO_REMOTE=${JSON.stringify(info.remote).slice(1, -1)}`);
 });
