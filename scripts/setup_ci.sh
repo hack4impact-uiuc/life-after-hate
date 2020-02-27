d9e9037beb7c6611158b2351b76bf6d9f00af4bf
Josh Byster <joshb1050@gmail.com>
2020-02-26 23:40:08 -0600

    Fixed CORS

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -9,3 +9,10 @@ echo MAPQUEST_URI=${MAPQUEST_URI} >> .env
 echo REACT_APP_MAPBOX_ACCESS_TOKEN=${REACT_APP_MAPBOX_ACCESS_TOKEN} >> .env
 echo REACT_APP_API_URI=${REACT_APP_API_URI} >> .env 
 echo FE_URI=${FE_URI} >> .env 
+
+# Copy over the .env file to the frontend directory so that when we do a production build for FE,
+# it sees all these environment variables as well. Since 
+# it's required that these environment variables are there at build time,
+# for example REACT_APP_API_URI will be able to 
+# be integrated into the production bundle of the app.
+cp .env ./frontend/.env
