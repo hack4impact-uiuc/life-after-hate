225e89897d2890404ad564324ac14b31ca8164a0
JoshBurke <joshDgreat37@gmail.com>
2020-03-09 15:42:22 -0500

    changed login to functional component, added redirect if authed

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button } from "reactstrap";
+import { connect } from "react-redux";
 
 import Logo from "./images/lah-logo.png";
 import GoogleLogo from "./images/google-logo.png";
@@ -7,24 +8,30 @@ import Signin from "./images/google-signin.png";
 import { getURLForEndpoint } from "../../utils/apiHelpers.js";
 
 import "./styles.scss";
+import { Redirect } from "react-router-dom";
 
-class Login extends Component {
-  render() {
-    return (
-      <div className="login">
-        <img id="lah-logo" src={Logo} alt="LAH Logo" />
-        <img id="google-logo" src={GoogleLogo} alt="Google Logo" />
-        <p>Sign in to Life After Hate with Google</p>
-        <Button
-          className="login-button"
-          color="link"
-          href={getURLForEndpoint("auth/login")}
-        >
-          <img id="sign-in" src={Signin} alt="signin" />
-        </Button>
-      </div>
-    );
+const Login = props => {
+  if (props.authed) {
+    return <Redirect to={{ pathname: "/" }} />;
   }
-}
+  return (
+    <div className="login">
+      <img id="lah-logo" src={Logo} alt="LAH Logo" />
+      <img id="google-logo" src={GoogleLogo} alt="Google Logo" />
+      <p>Sign in to Life After Hate with Google</p>
+      <Button
+        className="login-button"
+        color="link"
+        href={getURLForEndpoint("auth/login")}
+      >
+        <img id="sign-in" src={Signin} alt="signin" />
+      </Button>
+    </div>
+  );
+};
 
-export default Login;
+const MapStateToProps = state => ({
+  authed: state.auth.authenticated
+});
+
+export default connect(MapStateToProps)(Login);
