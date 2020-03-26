a2b38cd739c5353689fa84d3ad5ad0752acca36e
Angad Garg <angad.garg3@gmail.com>
2020-03-26 17:54:01 -0500

    fixing accidental commit

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ axios.interceptors.response.use(response => {
   return response;
 });
 
-app.use(cors({ origin: /192.168.99.100:\d{4}/, credentials: true }));
+app.use(cors({ origin: /localhost:\d{4}/, credentials: true }));
 app.use(morgan("dev"));
 
 mongoose.connect(process.env.DB_URI, {
