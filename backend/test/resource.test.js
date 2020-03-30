ad568295c71b101d72287c37c7e9af731dac0e71
Angad Garg <angad.garg3@gmail.com>
2020-03-30 17:57:43 -0500

    lint

--- backend/test/resource.test.js
+++ backend/test/resource.test.js
@@ -191,7 +191,7 @@ describe("PUT /resources", () => {
     const newResource = await Resource.findById(resourceId);
     expect(newResource.description).to.eq("new description");
     expect(didCheckIsAdmin()).to.be.true;
-    stub.restore()
+    stub.restore();
   });
 });
 
