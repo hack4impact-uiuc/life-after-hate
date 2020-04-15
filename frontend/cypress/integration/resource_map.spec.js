b7e3280c91d9d53f59e632fe20827470b8c3d8fa
Josh Byster <joshb1050@gmail.com>
2020-04-15 03:14:00 -0500

    Lint

--- frontend/cypress/integration/resource_map.spec.js
+++ frontend/cypress/integration/resource_map.spec.js
@@ -11,7 +11,7 @@ context("Resource Map", () => {
   });
 
   it("Popup functionality works as expected", () => {
-    cy.get("#deckgl-overlay").click(536, 288);
+    cy.get("#view-default-view").click(536, 288);
     cy.get("[data-cy=popup-title]")
       .should("contain.text", "Best Western Old Mill Inn")
       .and("be.visible");
@@ -21,12 +21,12 @@ context("Resource Map", () => {
 
   it("Different popups when clicking between resources", () => {
     // https://on.cypress.io/title
-    cy.get("#deckgl-overlay").click(536, 288);
+    cy.get("#view-default-view").click(536, 288);
     cy.get("[data-cy=popup-title]").should(
       "contain.text",
       "Best Western Old Mill Inn"
     );
-    cy.get("#deckgl-overlay").click(625, 347);
+    cy.get("#view-default-view").click(625, 347);
     cy.get("[data-cy=popup-title]").should(
       "not.contain.text",
       "Best Western Old Mill Inn"
@@ -34,7 +34,7 @@ context("Resource Map", () => {
   });
 
   it("Should show modal correctly on expanding popup", () => {
-    cy.get("#deckgl-overlay").click(536, 288);
+    cy.get("#view-default-view").click(536, 288);
     cy.get(".popup-max").click();
     cy.get(".modal-title")
       .should("be.visible")
@@ -44,9 +44,7 @@ context("Resource Map", () => {
       .first()
       .should("contain.value", "Best Western Old Mill Inn");
     // Test that it's editable
-    cy.get(".modal-input-field")
-      .first()
-      .type("Hello world!");
+    cy.get(".modal-input-field").first().type("Hello world!");
     cy.get(".close-button").click();
     // Ensure the popup is still open
     cy.get("[data-cy=popup-title]")
@@ -55,10 +53,10 @@ context("Resource Map", () => {
   });
 
   it("Closes modal upon clicking away", () => {
-    cy.get("#deckgl-overlay").click(536, 288);
+    cy.get("#view-default-view").click(536, 288);
     cy.get("[data-cy=popup-title]").should("be.visible");
     // Click somewhere random...
-    cy.get("#deckgl-overlay").click(900, 500);
+    cy.get("#view-default-view").click(900, 500);
     cy.get("[data-cy=popup-title]").should("not.be.visible");
   });
 });
