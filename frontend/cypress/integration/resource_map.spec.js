160cf3d1dfc86bf8cf3fc4099d64802ce052dbeb
Josh Byster <joshb1050@gmail.com>
2020-04-16 01:38:50 -0500

    Additional tests on resource map

--- frontend/cypress/integration/resource_map.spec.js
+++ frontend/cypress/integration/resource_map.spec.js
@@ -1,8 +1,8 @@
-// Though waits are almost always discouraged, we have no choice but to wait
-// For the click event listener to attach to the map view pins
 /* eslint-disable cypress/no-unnecessary-waiting */
 /// <reference types="Cypress" />
 
+// Though waits are almost always discouraged, we have no choice but to wait
+// For the click event listener to attach to the map view pins
 const WAIT_DURATION = 800;
 
 context("Resource Map", () => {
@@ -69,7 +69,7 @@ context("Resource Map", () => {
     cy.get(".close-button").click();
   });
 
-  it.only("Popup button functionality should work", () => {
+  it("Popup button functionality should work", () => {
     cy.get("#view-default-view").wait(WAIT_DURATION).click(536, 288);
     // Click the edit resource button
     cy.get(".popup > .card-action > [data-cy=card-resource-edit-btn]")
@@ -92,6 +92,26 @@ context("Resource Map", () => {
       .should("be.visible")
       .and("not.have.text", "Edit Resource");
     cy.get(".close-button").click();
+  });
+
+  it("Properly de-focuses resources on close", () => {
+    cy.get("#view-default-view").wait(WAIT_DURATION).click(536, 288);
+
+    // Make sure the popup is open
+    cy.get("[data-cy=popup-title]").should("be.visible");
+    // Make sure the close button is visible
+    cy.get(".expanded .top-card-close > .top-close-icon")
+      .should("be.visible")
+      .click();
+    // Popup should no longer be visible
+    cy.get("[data-cy=popup-title]").should("not.be.visible");
+
+    // Try again, but this time using the popup close
+    cy.get("#view-default-view").click(536, 288);
+
+    cy.get("[data-cy=popup-title]").should("be.visible");
+    cy.get(".mapboxgl-popup-close-button").should("be.visible").click();
+    cy.get("[data-cy=popup-title]").should("not.be.visible");
   });
 
   it("Closes modal upon clicking away", () => {
