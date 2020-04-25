bd93a023b18597f80b3fdd03a4e25c58c55a2a02
Polarpi <eugeniachen123@gmail.com>
2020-04-24 22:25:33 -0500

    update Map view markers for resource filtering

--- frontend/src/pages/MapView/Map/index.jsx
+++ frontend/src/pages/MapView/Map/index.jsx
@@ -5,7 +5,7 @@ import StaticMap, {
 } from "react-map-gl";
 import DeckGL from "@deck.gl/react";
 import { connect } from "react-redux";
-import { resourceSelector } from "../../../redux/selectors/resource";
+import { tagFilteredResourceSelector } from "../../../redux/selectors/resource";
 import {
   selectMapResource,
   clearMapResource,
@@ -146,7 +146,7 @@ const Map = ({ center, resources, selectMapResource, clearMapResource }) => {
 };
 
 const mapStateToProps = (state) => ({
-  resources: resourceSelector(state),
+  resources: tagFilteredResourceSelector(state),
   center: state.map.center,
 });
 
