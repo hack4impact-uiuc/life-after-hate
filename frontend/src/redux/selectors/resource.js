f76f23a16195bfaf33edafb9f389d0b39bdd8047
Polarpi <eugeniachen123@gmail.com>
2020-04-25 00:00:15 -0500

    remove console.log + fix tagMatch function

--- frontend/src/redux/selectors/resource.js
+++ frontend/src/redux/selectors/resource.js
@@ -7,13 +7,10 @@ export const resourceSelector = (state) => state.resources;
 export const tagFilteredResourceSelector = createSelector(
   [tagSelector, resourceSelector],
   (tags, resources) => {
-    console.log(resources);
     if (!resources) {
       return [];
     }
-    var tagMatch = function (resource) {
-      return tags.every((t) => resource.tags.includes(t));
-    };
+    const tagMatch = (resource) => tags.every((t) => resource.tags.includes(t));
     return resources.filter(tagMatch);
   }
 );
