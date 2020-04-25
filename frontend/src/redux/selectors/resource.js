18376e98ee91b888b44e6006806d9c9cfc580195
Polarpi <eugeniachen123@gmail.com>
2020-04-24 21:36:51 -0500

    add tagFilteredResourceSelector to filter resources by tags

--- frontend/src/redux/selectors/resource.js
+++ frontend/src/redux/selectors/resource.js
@@ -1,1 +1,19 @@
+import { createSelector } from "reselect";
+
+import { tagSelector } from "./tags";
+
 export const resourceSelector = (state) => state.resources;
+
+export const tagFilteredResourceSelector = createSelector(
+  [tagSelector, resourceSelector],
+  (tags, resources) => {
+    console.log(resources);
+    if (!resources) {
+      return [];
+    }
+    var tagMatch = function (resource) {
+      return tags.every((t) => resource.tags.includes(t));
+    };
+    return resources.filter(tagMatch);
+  }
+);
