e0d95fb9baca6eb7224bb0400858be07bb267283
Aryn Harmon <achcello@gmail.com>
2020-04-25 22:47:37 -0700

    use  for env variables, add readme

--- scripts/db_backup/deploy.sh
+++ scripts/db_backup/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-aws s3api create-bucket --bucket $MONGODB_BACKUP_BUCKET --region us-east-1 && \
+aws s3api create-bucket --bucket ${MONGODB_BACKUP_BUCKET} --region us-east-1 && \
 sam deploy --template-file template.yml --stack-name lah-db-backup \
-  --capabilities CAPABILITY_IAM --s3-bucket $MONGODB_BACKUP_BUCKET \
-  --parameter-overrides S3Bucket=$MONGODB_BACKUP_BUCKET MongoURI=$DB_URI
+  --capabilities CAPABILITY_IAM --s3-bucket ${MONGODB_BACKUP_BUCKET} \
+  --parameter-overrides S3Bucket=${MONGODB_BACKUP_BUCKET} MongoURI=${DB_URI}
