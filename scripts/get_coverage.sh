696606b8e066dda6aea9917eb57a29d569be5416
Josh Byster <joshb1050@gmail.com>
2020-05-04 04:02:53 -0500

    Full test

--- scripts/get_coverage.sh
+++ scripts/get_coverage.sh
@@ -1,4 +1,4 @@
 mkdir backend/.nyc_output
 curl http://localhost:5000/__coverage__ > backend/.nyc_output/out.json
-docker-compose -f docker-compose.cypress.yml run -v "$(pwd)"/backend:/var/temp prod-backend /var/www/app/node_modules/.bin/nyc report --report-dir /var/temp/cover --temp-dir /var/temp/.nyc_output --reporter=lcov --reporter=clover --reporter=json
+$(npm bin)/nyc report --report-dir ./frontend/cover --temp-dir ./frontend/.nyc_output --reporter=lcov --reporter=clover --reporter=json
 docker-compose -f docker-compose.cypress.yml run -v "$(pwd)"/frontend:/var/temp prod-frontend /var/www/app/node_modules/.bin/nyc report --report-dir /var/temp/cover --temp-dir /var/temp/.nyc_output --reporter=lcov --reporter=clover --reporter=json
