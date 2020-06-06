93f9dce527c6fe52649b49146b9a7427f6e93caf
Josh Byster <joshb1050@gmail.com>
2020-06-06 03:57:02 -0500

    Added selector memoization for performance

--- frontend/src/redux/selectors/resource.js
+++ frontend/src/redux/selectors/resource.js
@@ -1,49 +1,45 @@
 import { resourceEnum, sortFieldEnum } from "../../utils/enums";
-
+import { createSelector } from "reselect";
 const resourceName = (resource) =>
   (resource.type === resourceEnum.INDIVIDUAL
     ? resource.contactName
-    : resource.companyName) || "";
+    : resource.companyName) ?? "";
 
-const resourceLocation = (resource) => resource.address || "";
+const resourceLocation = (resource) => resource.address ?? "";
 
-const resourcePointOfContact = (resource) => resource.contactEmail || "";
+const resourcePointOfContact = (resource) => resource.contactEmail ?? "";
 
 const resourceDescription = (resource) =>
   (resource.type === resourceEnum.INDIVIDUAL
     ? resource.skills
-    : resource.description) || "";
+    : resource.description) ?? "";
 
-export const resourceSelector = (state) => {
-  const resources = [...state.resources];
-  resources.sort((resource1, resource2) => {
-    const order = state.sort.order === "asc" ? 1 : -1;
-    switch (state.sort.field) {
-      case sortFieldEnum.RESOURCE_NAME:
-        return (
-          resourceName(resource1).localeCompare(resourceName(resource2)) * order
-        );
-      case sortFieldEnum.LOCATION:
-        return (
-          resourceLocation(resource1).localeCompare(
-            resourceLocation(resource2)
-          ) * order
-        );
-      case sortFieldEnum.POINT_OF_CONTACT:
-        return (
-          resourcePointOfContact(resource1).localeCompare(
-            resourcePointOfContact(resource2)
-          ) * order
-        );
-      case sortFieldEnum.DESCRIPTION:
-        return (
-          resourceDescription(resource1).localeCompare(
-            resourceDescription(resource2)
-          ) * order
-        );
-      default:
-        return 0;
-    }
-  });
-  return resources;
+const sortComparatorDict = {
+  [sortFieldEnum.RESOURCE_NAME]: resourceName,
+  [sortFieldEnum.RESOURCE_LOCATION]: resourceLocation,
+  [sortFieldEnum.POINT_OF_CONTACT]: resourcePointOfContact,
+  [sortFieldEnum.DESCRIPTION]: resourceDescription,
 };
+
+// Memoize sort selector to avoid extra computation
+export const resourceSelector = createSelector(
+  [
+    (state) => state.resources,
+    (state) => state.sort.order,
+    (state) => state.sort.field,
+  ],
+  (resources, order, field) => {
+    const customComparator = sortComparatorDict[field];
+    if (!customComparator) {
+      return resources;
+    }
+    const resourceCopy = [...resources];
+    const numericalOrder = order === "asc" ? 1 : -1;
+    resourceCopy.sort(
+      (resource1, resource2) =>
+        customComparator(resource1).localeCompare(customComparator(resource2)) *
+        numericalOrder
+    );
+    return resourceCopy;
+  }
+);
