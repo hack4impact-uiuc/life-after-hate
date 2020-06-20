aa429bd82a0650a0a8142d87399f2cefe9afa9dc
Josh Byster <joshb1050@gmail.com>
2020-06-20 01:01:57 -0500

    Hotfix cookie maxAge

--- backend/app.js
+++ backend/app.js
@@ -71,7 +71,9 @@ app.use(
     }),
     saveUninitialized: false, // don't create session until something stored
     resave: false, //don't save session if unmodified
-    maxAge: 10080000, // 1 week long
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week long
+    },
   })
 );
 
